8a812efd2cbbe8e21a3cc1a545df84e00b671ccb
ochieng24 <edwine.ochi24@gmail.com>
2023-05-09 10:58:59 -0700

    hot fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,31 +11,31 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-        int fd, written, len = 0;
+	int fd, written, len = 0;
 
-        if (!filename)
-                return (-1);
+	if (!filename)
+		return (-1);
 
-        if (text_content)
-        {
-                while (text_content[len])
-                        len++;
-        }
+	if (text_content)
+	{
+		while (text_content[len])
+			len++;
+	}
 
-        fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-        if (fd < 0)
-                return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd < 0)
+		return (-1);
 
-        if (text_content)
-        {
-                written = write(fd, text_content, len);
-                if (written != len)
-                {
-                        close(fd);
-                        return (-1);
-                }
-        }
+	if (text_content)
+	{
+		written = write(fd, text_content, len);
+		if (written != len)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
 
-        close(fd);
-        return (1);
+	close(fd);
+	return (1);
 }
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,28 +9,28 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-        int fd, bytes_written = 0, len = 0;
+	int fd, bytes_written = 0, len = 0;
 
-        if (filename == NULL)
-                return (-1);
+	if (filename == NULL)
+		return (-1);
 
-        if (text_content == NULL)
-                return (access(filename, F_OK) == 0 ? 1 : -1);
+	if (text_content == NULL)
+		return (access(filename, F_OK) == 0 ? 1 : -1);
 
-        fd = open(filename, O_WRONLY | O_APPEND);
-        if (fd == -1)
-                return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
 
-        while (text_content[len])
-                len++;
+	while (text_content[len])
+		len++;
 
-        bytes_written = write(fd, text_content, len);
-        if (bytes_written == -1 || bytes_written != len)
-        {
-                close(fd);
-                return (-1);
-        }
+	bytes_written = write(fd, text_content, len);
+	if (bytes_written == -1 || bytes_written != len)
+	{
+		close(fd);
+		return (-1);
+	}
 
-        close(fd);
-        return (1);
+	close(fd);
+	return (1);
 }
