b44e80f38e09819902c256ce065b18ecaf4a6a06
Ochieng24 <edwine.ochi24@gmail.com>
2023-08-17 11:28:15 -0700

    correction

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
 * print_dlistint - Prints all the elements of a dlistint_t list.
-* @h: Pointer to the head of the doubly linked list.
+* @head: Pointer to the head of the doubly linked list.
 *
 * Return: The number of nodes in the list.
 */
